725347c1c00b6fbf30434400819553f61d25e356
Divan <divanmeyer5@gmail.com>
2026-02-19 13:34:13 +0200

    Initial commit- Project Scaffold, Project References and Domain layer

---
+++ src/BrunoVehicleHire.Domain/Entities/Vehicle.cs
@@ -0,0 +1,12 @@
+namespace BrunoVehicleHire.Domain.Entities;
+
+public class Vehicle
+{
+    public Guid Id { get; set; }
+    public required string RegistrationNumber { get; set; } 
+    public required string Make { get; set; }
+    public required string Model { get; set; } 
+    public required string Year { get; set; } 
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
---
+++ src/BrunoVehicleHire.Domain/Interfaces/IVehicleRepository.cs
@@ -0,0 +1,13 @@
+using BrunoVehicleHire.Domain.Entities;
+
+namespace BrunoVehicleHire.Domain.Interfaces;
+
+public interface IVehicleRepository
+{
+    Task<Vehicle?> GetByIdAsync(Guid vehicleId);
+    Task<Vehicle?> GetByRegistrationNumberAsync(string registrationNumber);
+    Task<(List<Vehicle> Items, int TotalCount)> GetPaginatedAsync(int pageNumber, int pageSize, string searchTerm);
+    Task<Guid> AddAsync(Vehicle vehicle);
+    Task<Vehicle> UpdateAsync(Vehicle vehicle);
+    Task<bool> DeleteAsync(Guid guid);
+}
